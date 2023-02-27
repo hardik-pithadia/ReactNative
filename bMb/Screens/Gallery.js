cb574e1bda3d3aca23c723f728abdf736efa85b4
hardik-pithadia <67457334+hardik-pithadia@users.noreply.github.com>
2023-02-27 12:31:51 +0530

    added gallery images

--- bMb/Screens/Gallery.js
+++ bMb/Screens/Gallery.js
@@ -10,42 +10,27 @@ import {FlatGrid} from 'react-native-super-grid';
 
 const Gallery = () => {
   const [items, setItems] = useState([
-    {id: 0, name: require('../Images/demoImage1.png'), title: 'Directory'},
-    {id: 1, name: require('../Images/demoImage1.png'), title: 'Calendar'},
-    {id: 2, name: require('../Images/demoImage1.png'), title: 'Events'},
-    {id: 3, name: require('../Images/demoImage1.png'), title: 'About Us'},
-    {id: 4, name: require('../Images/demoImage1.png'), title: 'Gallery'},
-    {id: 5, name: require('../Images/demoImage1.png'), title: 'Join Us'},
-    {id: 6, name: require('../Images/demoImage1.png'), title: 'Member Feeds'},
-    {id: 7, name: require('../Images/demoImage1.png'), title: 'Sponsers'},
-    {id: 8, name: require('../Images/demoImage1.png'), title: 'Contact Us'},
-    {id: 9, name: require('../Images/demoImage1.png'), title: 'Directory'},
-    {id: 10, name: require('../Images/demoImage1.png'), title: 'Calendar'},
-    {id: 11, name: require('../Images/demoImage1.png'), title: 'Events'},
-    {id: 12, name: require('../Images/demoImage1.png'), title: 'About Us'},
-    {id: 13, name: require('../Images/demoImage1.png'), title: 'Gallery'},
-    {id: 14, name: require('../Images/demoImage1.png'), title: 'Join Us'},
-    {id: 15, name: require('../Images/demoImage1.png'), title: 'Member Feeds'},
-    {id: 16, name: require('../Images/demoImage1.png'), title: 'Sponsers'},
-    {id: 17, name: require('../Images/demoImage1.png'), title: 'Contact Us'},
-    {id: 18, name: require('../Images/demoImage1.png'), title: 'Directory'},
-    {id: 19, name: require('../Images/demoImage1.png'), title: 'Calendar'},
-    {id: 20, name: require('../Images/demoImage1.png'), title: 'Events'},
-    {id: 21, name: require('../Images/demoImage1.png'), title: 'About Us'},
-    {id: 22, name: require('../Images/demoImage1.png'), title: 'Gallery'},
-    {id: 23, name: require('../Images/demoImage1.png'), title: 'Join Us'},
-    {id: 24, name: require('../Images/demoImage1.png'), title: 'Member Feeds'},
-    {id: 25, name: require('../Images/demoImage1.png'), title: 'Sponsers'},
-    {id: 26, name: require('../Images/demoImage1.png'), title: 'Contact Us'},
-    {id: 27, name: require('../Images/demoImage1.png'), title: 'Directory'},
-    {id: 28, name: require('../Images/demoImage1.png'), title: 'Calendar'},
-    {id: 29, name: require('../Images/demoImage1.png'), title: 'Events'},
-    {id: 30, name: require('../Images/demoImage1.png'), title: 'About Us'},
-    {id: 31, name: require('../Images/demoImage1.png'), title: 'Gallery'},
-    {id: 32, name: require('../Images/demoImage1.png'), title: 'Join Us'},
-    {id: 33, name: require('../Images/demoImage1.png'), title: 'Member Feeds'},
-    {id: 34, name: require('../Images/demoImage1.png'), title: 'Sponsers'},
-    {id: 35, name: require('../Images/demoImage1.png'), title: 'Contact Us'},
+    {id: 0, name: require('../Images/Gallery101.jpeg'), title: 'Directory'},
+    {id: 1, name: require('../Images/Gallery102.jpeg'), title: 'Calendar'},
+    {id: 2, name: require('../Images/Gallery103.jpeg'), title: 'Events'},
+    {id: 3, name: require('../Images/Gallery104.jpeg'), title: 'About Us'},
+    {id: 4, name: require('../Images/Gallery105.jpeg'), title: 'Gallery'},
+    {id: 5, name: require('../Images/Gallery106.jpeg'), title: 'Join Us'},
+    {id: 6, name: require('../Images/Gallery107.jpeg'), title: 'Member Feeds'},
+    {id: 7, name: require('../Images/Gallery108.jpeg'), title: 'Sponsers'},
+    {id: 8, name: require('../Images/Gallery109.jpeg'), title: 'Contact Us'},
+    {id: 9, name: require('../Images/Gallery110.jpeg'), title: 'Directory'},
+    {id: 10, name: require('../Images/Gallery111.jpeg'), title: 'Calendar'},
+    {id: 11, name: require('../Images/Gallery112.jpeg'), title: 'Events'},
+    {id: 12, name: require('../Images/Gallery113.jpeg'), title: 'About Us'},
+    {id: 13, name: require('../Images/Gallery114.jpeg'), title: 'Gallery'},
+    {id: 14, name: require('../Images/Gallery115.jpeg'), title: 'Join Us'},
+    {id: 15, name: require('../Images/Gallery116.jpeg'), title: 'Member Feeds'},
+    {id: 16, name: require('../Images/Gallery117.jpeg'), title: 'Sponsers'},
+    {id: 17, name: require('../Images/Gallery118.jpeg'), title: 'Contact Us'},
+    {id: 18, name: require('../Images/Gallery119.jpeg'), title: 'Directory'},
+    {id: 19, name: require('../Images/Gallery120.jpeg'), title: 'Calendar'},
+    {id: 20, name: require('../Images/Gallery121.jpeg'), title: 'Events'},
   ]);
   return (
     <View
@@ -56,7 +41,7 @@ const Gallery = () => {
         marginBottom: 25,
       }}>
       <FlatGrid
-        itemDimension={80}
+        itemDimension={120}
         data={items}
         style={styles.gridView}
         spacing={15}
@@ -67,7 +52,7 @@ const Gallery = () => {
               resizeMode="stretch"
               style={{
                 borderRadius: 10,
-                height: 80,
+                height: 120,
                 overflow: 'hidden',
               }}>
               <TouchableOpacity
